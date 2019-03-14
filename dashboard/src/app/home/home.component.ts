d729a77d5691e62e8c231e42ec1fbdb50af5b0f4
magrosskopf <magrosskopf@web.de>
2019-03-14 15:39:35 +0100

    testet some gridster shit

--- dashboard/src/app/home/home.component.ts
+++ dashboard/src/app/home/home.component.ts
@@ -66,7 +66,7 @@ export class HomeComponent implements OnInit {
       resizable: {
         enabled: true,
       },
-      swap: false,
+      swap: true,
       pushItems: true,
       disablePushOnDrag: false,
       disablePushOnResize: false,
@@ -88,7 +88,7 @@ export class HomeComponent implements OnInit {
     {cols: 2, rows: 2, y: 3, x: 5, minItemRows: 2, minItemCols: 2, label: 'Min rows & cols = 2'},
     {cols: 2, rows: 2, y: 2, x: 0, maxItemRows: 2, maxItemCols: 2, label: 'Max rows & cols = 2'},
     {cols: 2, rows: 1, y: 2, x: 2, dragEnabled: true, resizeEnabled: true, label: 'Drag&Resize Enabled'},
-    {cols: 1, rows: 1, y: 2, x: 4, dragEnabled: false, resizeEnabled: false, label: 'Drag&Resize Disabled'},
+    {cols: 1, rows: 1, y: 2, x: 4, dragEnabled: true, resizeEnabled: false, label: 'Drag&Resize Disabled'},
     {cols: 1, rows: 1, y: 2, x: 6}
   ];
   }
