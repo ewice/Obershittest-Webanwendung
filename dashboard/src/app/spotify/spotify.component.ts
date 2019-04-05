5004217bf6738484c68f4358ee0f50c7f6dda228
SirLocke <vfb.fan4ever.nm@googlemail.com>
2019-04-05 11:23:32 +0200

    implemented Toggle for PLay/resume

--- dashboard/src/app/spotify/spotify.component.ts
+++ dashboard/src/app/spotify/spotify.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router, Params } from "@angular/router";
+let toggle:Boolean;
+      toggle =true;
 
 @Component({
   selector: 'app-spotify',
@@ -14,25 +16,39 @@ export class SpotifyComponent implements OnInit {
     private httpClient: HttpClient) {
 
 
+
   }
 
   ngOnInit() {
   }
+togglePLay(){
 
 
+  if (toggle === true){
+    this.play();
+    toggle = false;
+    
 
-  playPause() {
+  }else if ( toggle === false){
+  this.pause();
+  toggle = true;
+
+
+}
+}
+
+
+  play() {
     let token: String;
     this.route.fragment.subscribe(fragment => {
       token = fragment.substring(fragment.indexOf('access_token=') + 13, fragment.indexOf('&', 0));
       const httpOptions = {
-       headers: new HttpHeaders({
-         'Authorization': 'Bearer ' + "BQC8UY9vfe_nSnnPFg5scdB8bYUt13U_UEWthm0E78kIn29ZzwW2wsqAKryDPOVnQcPY3jWUKUiE5PfrYkcTXUQ0L2vYdsdDx0bWQ-UOEKCiTuuF0kIZ4sHygI_kt_ZR8G05_u2rpo6Fqsm6RGL0JSt3qqTBZ3ER28k"
-       })
+        headers: new HttpHeaders({
+          'Authorization': 'Bearer ' + token
+        })
       };
-//console.log(httpOptions);
 
-      this.httpClient.put('https://api.spotify.com/v1/me/player/play', "{} ", httpOptions)
+      this.httpClient.put('https://api.spotify.com/v1/me/player/play', "{}", httpOptions)
         .subscribe(
           data => {
 
@@ -45,16 +61,32 @@ export class SpotifyComponent implements OnInit {
         );
     })
 
-   // const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
-   //let headers = new Headers();
-   //headers.set("Authorization", "Bearer " +token)
-    console.log(token)
-    //const httpHeaders = new HttpHeaders ({
-    //  'Authorization': 'Bearer ' + token
-    //});
 
 
   }
+  pause() {
+    let token: String;
+    this.route.fragment.subscribe(fragment => {
+      token = fragment.substring(fragment.indexOf('access_token=') + 13, fragment.indexOf('&', 0));
+      const httpOptions = {
+        headers: new HttpHeaders({
+          'Authorization': 'Bearer ' + token
+        })
+      };
+
+      this.httpClient.put('https://api.spotify.com/v1/me/player/pause', "", httpOptions)
+        .subscribe(
+          data => {
+
+            console.log('PUT Request is successful ', data);
+          },
+          error => {
+            console.log('Error', error);
+
+          }
+        );
+    });
+
+  }
+
 }
-
-
